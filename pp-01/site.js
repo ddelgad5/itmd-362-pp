956e6477f028a4a862650bb90ed4880ad0b90425
David Delgado <ddelgad5@hawk.iit.edu>
2018-01-26 11:20:05 -0600

    Add comments.

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,10 +1,13 @@
 $('#form').on("submit", function(e) {
   e.preventDefault(); //Prevent link
   console.log("Form Submitted"); //Add to console
-  var today = new Date();
-  var bdate = $('#date')[0].valueAsDate;
-  var tyear = today.getFullYear();
-  var byear = bdate.getFullYear();
-  console.log("Today's date is " + today);
-
+  var today = new Date(); //Get todays date
+  var bdate = $('#date')[0].valueAsDate; //Get user DOB
+  var tyear = today.getFullYear(); //Get todays year
+  var byear = bdate.getFullYear(); //Get users YOB
 });
+
+function formsubmitted() {
+  console.log("Form passed proper tests");
+  
+}
