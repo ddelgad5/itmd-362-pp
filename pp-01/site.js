1440343c6aaa52621ab2edfb568a677ccda8ad07
David Delgado <ddelgad5@hawk.iit.edu>
2018-01-26 11:10:13 -0600

    Declare vars for user input, todays date, and cand corresponding years.

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,4 +1,10 @@
 $('#form').on("submit", function(e) {
   e.preventDefault(); //Prevent link
   console.log("Form Submitted"); //Add to console
+  var today = new Date();
+  var bdate = $('#date')[0].valueAsDate;
+  var tyear = today.getFullYear();
+  var byear = bdate.getFullYear();
+  console.log("Today's date is " + today);
+
 });
